51874e7b3d45efefd4b80f3382d821502e6e999a
VagiSingh <171151040+VagiSingh@users.noreply.github.com>
2025-06-07 14:00:08 +0530

    gets Player choice

--- app.js
+++ app.js
@@ -9,8 +9,13 @@ else if (random>0.33 &&random<=0.66){
    return "paper";
 }
 else{
-    return "scissors"
+    return "scissors";
 }
 };
 
 
+const playerSelection = function getPlayerChoice(){
+  const enteredValue = prompt("Enter your choice : Rock , Paper or Scissors.")
+  const playerChoice= enteredValue.toLowerCase();
+  return playerChoice;
+} 
