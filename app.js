dcd993d2d28b1a055c74370b170bba0e2a0a47d4
VagiSingh <171151040+VagiSingh@users.noreply.github.com>
2025-06-07 17:23:30 +0530

    plays complete rock-paper-scissor 5 rounds with computer

--- app.js
+++ app.js
@@ -19,3 +19,60 @@ const playerSelection = function getPlayerChoice(){
   const playerChoice= enteredValue.toLowerCase();
   return playerChoice;
 } 
+let computerPoint=0; let playerPoint=0;
+
+function gameRound(computerChoice, playerChoice){
+  if(computerChoice==playerChoice){
+    alert("It was a Tie!");
+    return;
+  }
+  else if (computerChoice=="rock" && playerChoice == "paper"){
+    alert("You Won!🙌")
+    playerPoint ++;
+    return;
+  }
+  else if (computerChoice=="paper" && playerChoice == "scissors"){
+    alert("You Won!🙌")
+    playerPoint ++;
+    return;
+  }
+  else if (computerChoice=="scissors" && playerChoice == "rock"){
+    alert("You Won!🙌")
+    playerPoint ++;
+    return;
+  }
+  else if (computerChoice=="scissors" && playerChoice == "paper"){
+    alert("You lost.😢")
+    computerPoint ++;
+    return;
+  }
+  else if (computerChoice=="rock" && playerChoice == "scissors"){
+    alert("You lost.😢")
+    computerPoint ++;
+    return;
+  }
+  else if (computerChoice=="paper" && playerChoice == "rock"){
+    alert("You lost.😢")
+    computerPoint ++;
+    return;
+  }
+
+}
+console.log(playerPoint);
+console.log(computerPoint);
+
+function playGame(){
+  while(playerPoint + computerPoint!=5){
+    gameRound(computerSelection(),playerSelection());
+  }
+  if(playerPoint>computerPoint){
+    alert("Congratulation!You Won!🎉 Computer's Score:"+computerPoint+" Your Score:"+playerPoint);
+    return;
+  }
+  else{
+    alert("Computer Won.Better luck next time. Computer's Score:"+computerPoint+" Your Score:"+playerPoint);
+    return;
+  }
+}
+
+playGame()
